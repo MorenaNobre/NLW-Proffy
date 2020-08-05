5ed2333e21c9ea300ba601dbb80e593eb02734ac
MorenaNobre <morenagnobre@gmail.com>
2020-08-05 08:49:41 -0300

    PageHeader

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
 
-function PageHeader() {
-    return (
-        <header className="page-header">
-            <div className="top-bar-container">
-                <Link to="/">
-                    <img src={backIcon} alt="Proffy" />
-                </Link>
-                <img src={logoImg} alt="Voltar" />
-                    
-            </div>
-
-            <div className="header-content">
-                <strong>Estes são os proffys disponíveis.</strong>
-            </div>
-        </header>
-    )
+interface PagerHeaderProps {
+  title: string
 }
 
-export default PageHeader
+const PageHeader: React.FC<PagerHeaderProps> = (props) => {
+  return (
+    <header className="page-header">
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Proffy" />
+        </Link>
+        <img src={logoImg} alt="Voltar" />
+      </div>
+
+      <div className="header-content">
+        <strong>{props.title}</strong>
+        
+        {props.children}
+      </div>
+
+    </header>
+  );
+}
+
+export default PageHeader;
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import PageHeader from "../../components/PageHeader";
 
 // import { Container } from './styles';
 
 function TeacherForm() {
   return (
-      <h1>Teacher Form</h1>
-  )
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas."/>
+    </div>
+  );
 }
 
 export default TeacherForm;
