eef62c42998bfccf2f5a15d62d306377242ae6e4
MorenaNobre <morenagnobre@gmail.com>
2020-08-05 07:48:17 -0300

    taecherList/teacherForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+
+import Routes from './routes';
+
 import './assets/styles/global.css'
-import Landing from './pages/Landing'
+
 
 function App() {
   return (
-    <Landing />
+    <Routes />
   );
 }
 
 export default App;
-//componente dentro do React
